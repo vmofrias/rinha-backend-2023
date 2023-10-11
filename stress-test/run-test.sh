85a463f293f52b1ff89bd3c00414b834cc2051b8
Vinicius Frias <vmofrias@gmail.com>
2023-10-11 20:04:21 -0300

    Retirado request para contagem-pessoas após teste de stress

--- stress-test/run-test.sh
+++ stress-test/run-test.sh
@@ -13,7 +13,3 @@ sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendSimulation \
     -rf $WORKSPACE/user-files/results \
     -sf $WORKSPACE/user-files/simulations \
     -rsf $WORKSPACE/user-files/resources \
-
-sleep 3
-
-curl -v "http://localhost:9999/contagem-pessoas"
